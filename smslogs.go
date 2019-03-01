79090e188c215c85b2b828714cb87e1951f5af5c
patomp3 <45219240+patomp3@users.noreply.github.com>
2019-03-01 15:38:28 +0700

    Create smslogs.go

---
+++ smslogs.go
@@ -0,0 +1,114 @@
+package log
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+//LogInfo for ..
+type LogInfo struct {
+	Timestamp       string   `json:"@timestamp"`
+	Tags            []string `json:"tags"`
+	OrderNo         string   `json:"orderno"`
+	TrackingNo      string   `json:"trackingno"`
+	ApplicationName string   `json:"applicationname"`
+	FunctionName    string   `json:"functionname"`
+	OrderDate       string   `json:"orderdate"`
+	TVSNo           string   `json:"tvsno"`
+	MobileNo        string   `json:"mobileno"`
+	SerialNo        string   `json:"serialno"`
+	Reference1      string   `json:"reference1"`
+	Reference2      string   `json:"reference2"`
+	Reference3      string   `json:"reference3"`
+	Reference4      string   `json:"reference4"`
+	Reference5      string   `json:"reference5"`
+	Request         string   `json:"request"`
+	Response        string   `json:"response"`
+	Start           string   `json:"start"`
+	End             string   `json:"end"`
+	Duration        string   `json:"duration"`
+	Level           string   `json:"level"`
+}
+
+var debugLevel int
+
+// Level for debug
+type Level int
+
+const (
+	ALL Level = iota + 1
+	TRACE
+	DEBUG
+	INFO
+	WARN
+	ERROR
+)
+
+// New for create logInfo
+func New(l Level) *LogInfo {
+	//debugLevel := l
+	//fmt.Printf("debug level = %d\n", debugLevel)
+
+	return &LogInfo{Timestamp: time.Now().Format(time.RFC3339Nano), Level: getName(l)}
+}
+
+// PrintLog for send log to stdoutput
+func (l *LogInfo) PrintLog() {
+	jsonStr, _ := json.Marshal(l)
+	fmt.Printf("%s\n", string(jsonStr))
+}
+
+/*func (l *LogInfo) SubmitLog(params ...Level) {
+	//
+	total := 0
+	for _, num := range params {
+		total = total + 1
+		_ = num
+	}
+	if total > 0 {
+		l.Level = getName(params[0])
+	}
+	jsonStr, _ := json.Marshal(l)
+
+	fmt.Printf("%s\n", string(jsonStr))
+}*/
+
+/*func main() {
+
+	log := New(INFO)
+	log.PrintLog()
+
+	log.TVSNo = "6666666"
+	log.PrintLog()
+
+	tag := []string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	log.Tags = tag
+	log.PrintLog()
+	//displayLog(ALL, "Hello")
+}*/
+
+func getName(level Level) string {
+	var myReturn string
+
+	switch level {
+	case ALL:
+		myReturn = "ALL"
+	case TRACE:
+		myReturn = "TRACT"
+	case DEBUG:
+		myReturn = "DEBUG"
+	case INFO:
+		myReturn = "INFO"
+	case WARN:
+		myReturn = "WARN"
+	case ERROR:
+		myReturn = "ERROR"
+	}
+	return myReturn
+}
