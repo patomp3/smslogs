630baea4f8f44ced952f8f97893ae5784716a5fd
patomp3 <45219240+patomp3@users.noreply.github.com>
2021-08-18 17:13:05 +0700

    1

--- smslogs.go
+++ smslogs.go
@@ -33,7 +33,31 @@ type LogInfo struct {
 	Suffix          string `json:"@suffix"`
 }
 
-var debugLevel int
+//LogInfoE2E for ..
+type LogInfoE2E struct {
+	Prefix    string `json:"@prefix"`
+	Suffix    string `json:"@suffix"`
+	Timestamp string `json:"@timestamp"`
+
+	Level           string  `json:"level"`
+	LogCat          string  `json:"log_cate"`
+	TxId            string  `json:"tx_id"`
+	StepTxId        string  `json:"step_txid"`
+	StepName        string  `json:"step_name"`
+	EndpointName    string  `json:"endpoint_name"`
+	Endpoint        string  `json:"endpoint"`
+	ResultCode      string  `json:"result_code"`
+	ResultDesc      string  `json:"result_desc"`
+	StartDate       string  `json:"start_date"`
+	EndDate         string  `json:"end_date"`
+	ElapsedTime     float64 `json:"elapsed_time"`
+	ResultIndicator string  `json:"result_indicator"`
+
+	Request  string `json:"request"`
+	Response string `json:"response"`
+}
+
+//var debugLevel int
 
 // Level for debug
 type Level int
@@ -67,6 +91,30 @@ func (l *LogInfo) PrintLog(level Level, funcName string, correlationID string, r
 	l.Suffix = getName(level)
 	l.FunctionName = funcName
 	l.CorrelationID = correlationID
+	l.Request = req
+	l.Response = res
+
+	jsonStr, _ := json.Marshal(l)
+	fmt.Printf("%s\n", string(jsonStr))
+}
+
+// New for create logInfo
+func NewE2E(prefix string, suffix string, txid string) *LogInfoE2E {
+	//debugLevel := l
+	//fmt.Printf("debug level = %d\n", debugLevel)
+	log := &LogInfoE2E{Prefix: prefix, Suffix: suffix, TxId: txid}
+
+	return log
+}
+
+// Close dbinfo
+func (l *LogInfoE2E) CloseE2E() {
+	l = nil
+}
+
+// PrintLogE2E for send log to stdoutput
+func (l *LogInfo) PrintLogE2E(req string, res string) {
+	l.Timestamp = time.Now().Format(time.RFC3339Nano)
 	l.Request = req
 	l.Response = res
 
